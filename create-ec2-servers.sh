3669635bb889e4dfbc97d74415eccdb827dddd42
mettashalini89@gmail.com <CatMatDog@123>
2023-02-21 10:42:55 -0700

    create instance code update

--- create-ec2-servers.sh
+++ create-ec2-servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z02543141QBSJJNNYEJL"
-DOMAIN="devopsb71.live"
+ZONE_ID="Z103214126L48SQW30RSR"
+DOMAIN="devopsb71.online"
 SG_NAME="allow-all"
 #############################
 
