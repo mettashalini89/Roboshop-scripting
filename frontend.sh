0e30928170e2fc08d522db44a6dce2cca9f1c592
mettashalini89@gmail.com <CatMatDog@123>
2023-02-23 13:41:20 -0700

    status check using if

--- frontend.sh
+++ frontend.sh
@@ -2,34 +2,31 @@ source common.sh
 
 print_head "Installing Nginx"
 yum install nginx -y &>>${log_file}
-if [ $? -eq 0 ]; then
-  echo Success
-else
-  echo Failure
-fi
+status_check $?
 
 print_head "remove old content if any"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-echo $?
+status_check $?
 
 print_head "download tha content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo $?
+status_check $?
 
 print_head "move the content to html file"
 cd /usr/share/nginx/html &>>${log_file}
-echo $?
+status_check $?
 
 unzip /tmp/frontend.zip &>>${log_file}
-echo $?
+status_check $?
+
 print_head "copy the roboshop config file"
 cp ${code_dir}/Config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo $?
+status_check $?
 
 print_head "enable nginx service"
 systemctl enable nginx &>>${log_file}
-echo $?
+status_check $?
 
 print_head "start nginx service"
 systemctl start nginx &>>${log_file}
-echo $? #To get the status of previous command 0 is success 1-255 is failure
+status_check $? # echo $? To get the status of previous command 0 is success 1-255 is failure
