33c7131578b4f3d3e2a6221be81cfbca2973d6ea
mettashalini89@gmail.com <CatMatDog@123>
2023-02-23 10:58:24 -0700

    functions added to common file

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ print_head "install mongodb"
 yum install mongodb-org -y &>>${log_file}
 
 print_head "update  listen address from 127.0.0.1 to 0.0.0.0"
-sed -i -e "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>>$
+sed -i -e "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>>${log_file}
 
 print_head "Enable mongo server"
 systemctl enable mongod &>>${log_file}
