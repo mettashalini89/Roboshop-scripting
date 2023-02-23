5de517f888318242b4a82cafff66586569b7c434
mettashalini89@gmail.com <CatMatDog@123>
2023-02-23 11:11:16 -0700

    functions added to common file

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ print_head "install mongodb"
 yum install mongodb-org -y &>>${log_file}
 
 print_head "update  listen address from 127.0.0.1 to 0.0.0.0"
-sed -i -e "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>>${log_file}
+sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf &>>${log_file}
 
 print_head "Enable mongo server"
 systemctl enable mongod &>>${log_file}
@@ -15,4 +15,4 @@ systemctl enable mongod &>>${log_file}
 print_head "Start mongo server"
 systemctl start mongod &>>${log_file}
 
-#problem: need to update  listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
+
