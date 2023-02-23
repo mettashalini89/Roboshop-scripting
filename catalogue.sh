d53542d307734a76c0bebd6691bfc474d16dcfd1
mettashalini89@gmail.com <CatMatDog@123>
2023-02-23 14:06:02 -0700

    add user if not exit using condition

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,10 @@ yum install nodejs -y &>>${log_file}
 status_check $?
 
 print_head "add user roboshop"
-useradd roboshop &>>${log_file}
+id roboshop &>>${log_file}
+if [ $? -ne 0 ]; then
+  useradd roboshop &>>${log_file}
+fi
 status_check $?
 
 print_head "crate app directory/folder"
