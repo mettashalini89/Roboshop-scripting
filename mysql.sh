b1d496247c6e914f190b544e098c2992da7fd1c3
mettashalini89@gmail.com <CatMatDog@123>
2023-02-24 11:26:44 -0700

    my sql content

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
 source common.sh
 mysql_root_passwd=$1
 if [ -z "${mysql_root_passwd}" ]; then
-  echo -e "\e[31m sql root password is nissing \e[0m"
+  echo -e "\e[31m sql root password is missing \e[0m"
   exit 1
 fi
 
